3911281481620ad60365ef0613b8de27771fd030
Randy Findley <rgfindley@gmail.com>
2020-04-24 15:38:55 -0400

    clean up

--- src/lib/logger.js
+++
@@ -1,7 +0,0 @@
-const _ = require('lodash');
-const logger = require('pino')({
-  prettyPrint: _.includes(['development', 'test'], process.env.NODE_ENV),
-  level: _.includes(['development', 'test'], process.env.NODE_ENV) ? 'debug' : 'info',
-});
-
-module.exports = logger;
